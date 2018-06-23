3248582a5def0e0f4e2d3eafba24e9eafe2cd6b1
Romeo Maryns <romeo.maryns@gmail.com>
2018-06-23 21:37:34 +0200

    opkuis + cumstom on/off switches

--- modules/web/src/eu/maryns/romeo/kinderkankerfonds/web/persoon/PersoonEdit.java
+++ modules/web/src/eu/maryns/romeo/kinderkankerfonds/web/persoon/PersoonEdit.java
@@ -1,7 +1,57 @@
 package eu.maryns.romeo.kinderkankerfonds.web.persoon;
 
 import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.cuba.gui.components.CheckBox;
 import eu.maryns.romeo.kinderkankerfonds.entity.Persoon;
 
+import javax.inject.Inject;
+import java.util.Map;
+
 public class PersoonEdit extends AbstractEditor<Persoon> {
+
+    @Inject
+    private CheckBox actiefCheckbox;
+
+    @Inject
+    private CheckBox oudercomiteCheckbox;
+
+    @Inject
+    private CheckBox familieCheckbox;
+
+    @Inject
+    private CheckBox ontmoetingsdagCheckbox;
+
+    @Inject
+    private CheckBox raakpuntCheckbox;
+
+
+    @Override
+    protected void initNewItem(Persoon item) {
+        item.setActief(true);
+    }
+
+    @Override
+    public void init(Map<String, Object> params) {
+        com.vaadin.ui.CheckBox vaadinCheckBox = actiefCheckbox.unwrap(com.vaadin.ui.CheckBox.class);
+        vaadinCheckBox.setCaptionAsHtml(true);
+        vaadinCheckBox.setCaption("<span class=\"onoffswitch-inner\"></span><span class=\"onoffswitch-switch\"></span>");
+
+        com.vaadin.ui.CheckBox vaadin2CheckBox = oudercomiteCheckbox.unwrap(com.vaadin.ui.CheckBox.class);
+        vaadin2CheckBox.setCaptionAsHtml(true);
+        vaadin2CheckBox.setCaption("<span class=\"onoffswitch-inner\"></span><span class=\"onoffswitch-switch\"></span>");
+
+        com.vaadin.ui.CheckBox vaadin3CheckBox = familieCheckbox.unwrap(com.vaadin.ui.CheckBox.class);
+        vaadin3CheckBox.setCaptionAsHtml(true);
+        vaadin3CheckBox.setCaption("<span class=\"onoffswitch-inner\"></span><span class=\"onoffswitch-switch\"></span>");
+
+        com.vaadin.ui.CheckBox vaadin4CheckBox = ontmoetingsdagCheckbox.unwrap(com.vaadin.ui.CheckBox.class);
+        vaadin4CheckBox.setCaptionAsHtml(true);
+        vaadin4CheckBox.setCaption("<span class=\"onoffswitch-inner\"></span><span class=\"onoffswitch-switch\"></span>");
+
+        com.vaadin.ui.CheckBox vaadin5CheckBox = raakpuntCheckbox.unwrap(com.vaadin.ui.CheckBox.class);
+        vaadin5CheckBox.setCaptionAsHtml(true);
+        vaadin5CheckBox.setCaption("<span class=\"onoffswitch-inner\"></span><span class=\"onoffswitch-switch\"></span>");
+
+
+    }
 }
